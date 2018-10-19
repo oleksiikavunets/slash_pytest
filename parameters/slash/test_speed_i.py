586cfb830d7e1a7abc0c16f8dab4ee904caa9757
Alex <oleksiikavunets@gmail.com>
2018-10-19 17:03:13 +0300

    Init commit

---
+++ parameters/slash/test_speed_i.py
@@ -0,0 +1,121 @@
+import slash
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_a(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_b(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_c(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_d(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_q(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_w(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_e(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_r(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_t(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_y(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_u(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_i(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_o(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_p(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_s(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_f(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_g(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_h(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_j(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_k(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_l(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_z(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_x(x, y):
+    assert x == y
+
+
+@slash.parametrize(('x', 'y'), [(1, 1), (2, 2), (3, 3)])
+def test_speed_v(x, y):
+    assert x == y
