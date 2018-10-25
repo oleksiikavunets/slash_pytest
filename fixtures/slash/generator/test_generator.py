49bc15e963a53432f0f32935671305d8560d0cbd
ovpopova <ovpopova@luxoft.com>
2018-10-25 18:29:34 +0300

    ..

---
+++ fixtures/slash/generator/test_generator.py
@@ -0,0 +1,15 @@
+import slash
+
+local_ip = ['172.31.239.131',
+            '172.31.239.132',
+            '172.31.239.133']
+
+@slash.generator_fixture()
+def iter_yield():
+    for l in local_ip:
+        yield l
+        print("current ip: "+l)
+
+
+def test_yield():
+    iter_yield
