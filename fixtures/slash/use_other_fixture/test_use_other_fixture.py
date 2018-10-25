cf5e996a31099057a5582f66ee88e4b5e7d8dcac
kvitomanec6 <kmarko@luxoft.com>
2018-10-25 17:29:10 +0300

    added tests for comparison of requirements and other fixture usage fixtures

---
+++ fixtures/slash/use_other_fixture/test_use_other_fixture.py
@@ -0,0 +1,18 @@
+import slash
+
+
+@slash.fixture()
+def fixture_1():
+    print("\nfixture_1")
+
+
+@slash.fixture(autouse=True)
+def fixture_2(fixture_1):
+    print("\nfixture_2")
+
+
+class Tests(slash.Test):
+
+    # will use both fixtures
+    def test_1(self):
+        print("\ntest_1")
